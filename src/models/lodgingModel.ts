923f53ad3809c6767d575958cc7b8d0e283344d4
TonySN90 <tony.heider90@gmail.com>
2025-08-30 23:22:53 +0200

    add lodgingModel

---
+++ src/models/lodgingModel.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const lodgingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Lodging must have a name"],
+  },
+  maxNumGuests: {
+    type: Number,
+    required: [true, "Lodging must have a maximum number of guests"],
+  },
+  pricePerNight: {
+    type: Number,
+    required: [true, "Lodging must have a price per night"],
+  },
+  description: {
+    type: String,
+    required: [true, "Lodging must have a description"],
+  },
+  location: {
+    type: String,
+    required: [true, "Lodging must have a location"],
+  },
+  images: {
+    type: [String],
+    required: [true, "Lodging must have images"],
+  },
+  amenities: {
+    type: [String],
+    required: [true, "Lodging must have amenities"],
+  },
+  services: {
+    type: [String],
+    required: [true, "Lodging must have services"],
+  },
+  landlord: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Landlord",
+    required: [true, "Lodging must have a landlord"],
+  },
+});
+
+export default mongoose.model("Lodgings", lodgingSchema);
