4c2d7780271bc88ba4841956fb3adf396900100a
TonySN90 <tony.heider90@gmail.com>
2025-08-30 23:25:12 +0200

    add guests, landlords and lodging routes

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import { connectDB } from './src/config/database';
-import testRoutes from './src/routes/test';
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import testRoutes from "./src/routes/test";
+import landlordsRouter from "./src/routes/landlordsRouter";
+import { AppError } from "./src/utils/appError";
+import lodgingsRouter from "./src/routes/lodgingsRouter";
 
 const app: Application = express();
 
@@ -9,10 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/test', testRoutes);
+app.use("/api/v1/guests", testRoutes);
+app.use("/api/v1/landlords", landlordsRouter);
+app.use("/api/v1/lodgings", lodgingsRouter);
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Innovastay Backend API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Innovastay Backend API" });
 });
 
 export default app;
