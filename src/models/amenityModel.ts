1ce5c36340fc3ead3aff4b72239ebf6a299b21f4
TonySN90 <tony.heider90@gmail.com>
2025-08-31 11:52:41 +0200

    add amenity model

---
+++ src/models/amenityModel.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const amenitySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Amenity must have a name"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Amenity must have a description"],
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    // category: {
+    //   type: String,
+    //   required: [true, "Amenity must have a category"],
+    //   enum: ["comfort", "technology", "safety", "recreation", "accessibility", "other"],
+    // },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Amenity", amenitySchema);
