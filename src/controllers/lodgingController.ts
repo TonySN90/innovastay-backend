63466d10139c1b96d991a43fbf97eac3a7ff2d62
TonySN90 <tony.heider90@gmail.com>
2025-09-02 23:55:26 +0200

    update populating

--- src/controllers/lodgingController.ts
+++ src/controllers/lodgingController.ts
@@ -7,7 +7,9 @@ export const getAllLodgings = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const lodgings = await Lodging.find()
       .populate('amenities', 'name description')
-      .populate('services', 'name description');
+      .populate('services', 'name description')
+      .populate('rooms', 'name description quantity')
+      .populate('landlord', 'name');
 
     if (!lodgings || lodgings.length === 0) {
       next(new AppError("No lodgings found", 404));
@@ -30,7 +32,9 @@ export const createLodging = catchAsync(
     const newLodging = await Lodging.create(req.body);
     const lodging = await Lodging.findById(newLodging._id)
       .populate('amenities', 'name description')
-      .populate('services', 'name description');
+      .populate('services', 'name description')
+      .populate('rooms', 'name description quantity')
+      .populate('landlord', 'name');
 
     res.status(201).json({
       status: "success",
